daf0b0ae49c2a909a135adc2cd6ce5e1d65fe2cf
Spencer Rogers <spencer1248@gmail.com>
2013-10-31 15:33:57 -0400

    Add some tests and config capyabara

---
+++ spec/integration/home_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe "home" do
+  before do
+    visit "/"
+  end
+
+  describe "list" do
+    it "should display unfinished todo items" do
+      pending
+    end
+
+    it "should display finished todo items" do
+      pending
+    end
+
+    it "should link to a page for creating new todos" do
+      pending
+    end
+
+    it "should have a button for marking todo items as complete" do
+      pending
+    end
+
+    it "should have a button to edit existing todo items" do
+      pending
+    end
+  end
+end
